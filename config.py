6799dfc5418f117f7403a876d8c4055e16c3b9f7
Andres <andres@example.com>
2026-08-27 10:20:26 -0600

    Transpaso de PostgreeSQL de local a online a través de Render y arreglo de .env

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
